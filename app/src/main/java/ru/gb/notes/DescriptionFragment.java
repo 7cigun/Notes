472c2e26041cd88fa0eb29d0d257b7a8db31fb8d
7cigun <7cigun@gmail.com>
2022-02-28 17:37:40 +0300

    Урок 8. Добавил в свой custom toolbar меню из фрагмента.

--- app/src/main/java/ru/gb/notes/DescriptionFragment.java
+++ app/src/main/java/ru/gb/notes/DescriptionFragment.java
@@ -8,10 +8,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class DescriptionFragment extends Fragment {
@@ -29,6 +33,24 @@ public class DescriptionFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment_description,menu);
+        menu.findItem(R.id.action_about).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_toast: {
+                Toast.makeText(requireContext(),"Description Fragment", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_description, container, false);
@@ -37,6 +59,7 @@ public class DescriptionFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
         note = getArguments().getParcelable(KEY_NOTE);
         TextView descriptionView = new TextView(getContext());
         descriptionView.setTextSize(30f);
